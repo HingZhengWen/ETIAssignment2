5378ec1dabba581ec147277e413b6b5e103440de
Zheng Wen Hing <s10196494@connect.np.edu.sg>
2022-01-23 23:01:59 +0800

    update

--- main.go
+++ main.go
@@ -27,10 +27,15 @@ type Followers struct{
 type studentInfo struct {
 	Title string `json:"Student"`
 }
+func home(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Test")
+}
 
 func main(){
 	router := mux.NewRouter()
-	router.HandleFunc("/tutors", allStudents).Methods( "GET", "POST", "PUT", "DELETE")
+	router.HandleFunc("/students", allStudents).Methods( "GET", "POST", "PUT", "DELETE")
+	router.HandleFunc("welcome", home)
+
 	fmt.Println("Listening at port 8063")
 	log.Fatal(http.ListenAndServe(":8063",router))
 	db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/ETIStudentSocialDB")
